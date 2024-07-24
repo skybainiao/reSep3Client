8bc19447fa7dd05e094b970de3a1b214c7dcefb8
Junjie Chen <chen2468830035@gmail.com>
2024-07-24 14:37:08 +0200

    socket

---
+++ reSep3Client/Webservices/WebSocketService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace reSep3Client.Webservices
+{
+    public class WebSocketService
+    {
+        private ClientWebSocket _webSocket;
+
+        public async Task ConnectAsync(string uri)
+        {
+            _webSocket = new ClientWebSocket();
+            await _webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            await _webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public async Task<string> ReceiveMessageAsync()
+        {
+            var buffer = new byte[1024];
+            var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            return Encoding.UTF8.GetString(buffer, 0, result.Count);
+        }
+
+        public async Task CloseAsync()
+        {
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+        }
+    }
+}
